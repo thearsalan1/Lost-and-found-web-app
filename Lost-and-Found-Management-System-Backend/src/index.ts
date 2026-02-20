c3e53b9af7f6f8db0e77ac0d483c71548f4d1529
thearsalan1 <arsalanloq@gmail.com>
2026-02-20 23:19:41 +0530

    adding localhost to cors for development

--- Lost-and-Found-Management-System-Backend/src/index.ts
+++ Lost-and-Found-Management-System-Backend/src/index.ts
@@ -14,8 +14,8 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin:process.env.CORS_ORIGIN,
-  credentials:true
+  origin: [process.env.CORS_ORIGIN!, "http://localhost:5173"],
+  credentials: true
 }));
 app.use(express.json({limit:'10mb'}));
 app.use(express.urlencoded({extended:true}));
