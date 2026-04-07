98db2a679dbfbdfaa8fd97a7b031dcbbbfd86da4
thearsalan1 <arsalanloq@gmail.com>
2026-04-07 16:58:18 +0530

    Set email transporter to use secure connection

--- Lost-and-Found-Management-System-Backend/src/utils/emailService.ts
+++ Lost-and-Found-Management-System-Backend/src/utils/emailService.ts
@@ -2,9 +2,9 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT) || 587
+  port: Number(process.env.EMAIL_PORT) || 465
 ,
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
